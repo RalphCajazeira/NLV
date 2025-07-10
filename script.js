814bc82a2f19f5200cef7d7cff353732221d7645
Ralph Ramos <ralphmtk@gmail.com>
2025-07-10 14:15:25 -0300

    Project completed

--- script.js
+++ script.js
@@ -16,10 +16,37 @@ const questionAI = async (question, game, apiKey) => {
   const model = "gemini-2.5-flash";
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
   const prompt = `
-  olha, tenho esse jogo ${game} e queria saber: ${question}
+  ## Especialidade
+Você é um especialista assistente de meta para o jogo ${game}.
+
+  ## Tarefa
+  Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estrategias, build e dicas
+
+  ## Regras
+  - Se você não sabe a resposta, diga que não sabe, não tente inventar uma resposta.
+  - Se a pergunta não está relacionada ao jogo, diga que a pergunta não está relacionada ao jogo.
+  - Considere a data atual ${new Date().toLocaleDateString()}
+  - Faça pesquisa atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente
+  - Nunca responda itens que você não tenha certeza de que existe no patch atual
+  - Sempre responda em pt-br
+
+  ## Resposta
+  - Economize na resposta, seja direto e responda no máximo 1500 caracteres.
+  - Responda sempre em markdown
+  - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está buscando
+  - Responda conforme o game o tipo de resposta pode acabar mudando de acordo com o jogo nem todos jogos tem a mesma coisa, então o exemplo de resposta que dei pode não ser o mais correto.
+
+
+  ## Exemplo de resposta
+  pergunta: Qual é a melhor build para o jogo?
+  resposta: A build mais atual é:
+  ---
+  Aqui está a pergunta do usuário: ${question}
   `;
+
   const contents = [
     {
+      role: "user",
       parts: [
         {
           text: prompt,
@@ -27,6 +54,13 @@ const questionAI = async (question, game, apiKey) => {
       ],
     },
   ];
+
+  const tools = [
+    {
+      google_search: {},
+    },
+  ];
+
   // Chamada à API da IA
   const response = await fetch(geminiURL, {
     method: "POST",
@@ -35,6 +69,7 @@ const questionAI = async (question, game, apiKey) => {
     },
     body: JSON.stringify({
       contents,
+      tools,
     }),
   });
 
