375b26be673fa60ed2a8ebd2ea1fd5f75395a0f9
Ralph Ramos <ralphmtk@gmail.com>
2025-07-10 13:06:55 -0300

    Concluído post para o Gemini e resposta em HTML na pagina

--- script.js
+++ script.js
@@ -5,10 +5,48 @@ const askButton = document.getElementById("askButton");
 const aiResponse = document.getElementById("aiResponse");
 const form = document.getElementById("form");
 
-// Função para fazer a pergunta ao Gemini
-const questionAI = async () => {};
+// Função para converter o texto que vem em markdown em HTML
+const markdownToHTML = (text) => {
+  const converter = new showdown.Converter();
+  return converter.makeHtml(text);
+};
 
-form.addEventListener("submit", (event) => {
+// Função para fazer a pergunta para a IA
+const questionAI = async (question, game, apiKey) => {
+  const model = "gemini-2.5-flash";
+  const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
+  const prompt = `
+  olha, tenho esse jogo ${game} e queria saber: ${question}
+  `;
+  const contents = [
+    {
+      parts: [
+        {
+          text: prompt,
+        },
+      ],
+    },
+  ];
+  // Chamada à API da IA
+  const response = await fetch(geminiURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents,
+    }),
+  });
+
+  // Verificar se a resposta da API é válida
+  const data = await response.json();
+
+  // Exibir a resposta da IA
+  return data.candidates[0].content.parts[0].text;
+};
+
+// Função para lidar com o envio do formulário
+const sendForm = async (event) => {
   event.preventDefault();
 
   const apiKey = apiKeyInput.value;
@@ -26,6 +64,11 @@ form.addEventListener("submit", (event) => {
 
   try {
     // Perguntar para a IA
+    const text = await questionAI(question, game, apiKey);
+
+    // Exibir a resposta da IA no elemento HTML
+    aiResponse.querySelector(".response-content").innerHTML =
+      markdownToHTML(text);
   } catch (error) {
     console.error("Erro ao fazer a pergunta:", error);
     alert(
@@ -36,4 +79,7 @@ form.addEventListener("submit", (event) => {
     askButton.textContent = "Perguntar";
     askButton.classList.remove("loading");
   }
-});
+};
+
+// Adicionar o evento de envio do formulário
+form.addEventListener("submit", sendForm);
